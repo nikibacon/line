f0c18e297d354c9c3ea20cf37757265472c1a793
NIKI <niki801226@hotmail.com>
2020-08-14 15:24:17 +0800

    find pic

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, StickerSendMessage, FlexSendMessage
+    MessageEvent, TextMessage, TextSendMessage, StickerSendMessage, FlexSendMessage, ImageSendMessage
 )
 
 import configparser
