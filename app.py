b410ba08df4e364125295a88e5e2f136507f10b2
NIKI <niki801226@hotmail.com>
2020-08-14 15:15:52 +0800

    find pic

--- app.py
+++ app.py
@@ -48,30 +48,30 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    msg = event.message.text
-    r = '我看不懂你說什麼'
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     msg = event.message.text
+#     r = '我看不懂你說什麼'
 
-    if '貼圖' in msg:
-        sticker_message = StickerSendMessage(
-        package_id='1',
-        sticker_id='1'
-    )
-        line_bot_api.reply_message(
-            event.reply_token,
-            sticker_message)
-        return
+#     if '貼圖' in msg:
+#         sticker_message = StickerSendMessage(
+#         package_id='1',
+#         sticker_id='1'
+#     )
+#         line_bot_api.reply_message(
+#             event.reply_token,
+#             sticker_message)
+#         return
 
 
-    if msg in ['hi', 'Hi']:
-        r = 'hi'
-    elif msg == '你吃飯了嗎':
-        r = '還沒'
-    elif msg == '你是誰':
-        r = '我是機器人'
-    elif '訂位' in msg:
-        r = '你想訂位,是嗎?'
+#     if msg in ['hi', 'Hi']:
+#         r = 'hi'
+#     elif msg == '你吃飯了嗎':
+#         r = '還沒'
+#     elif msg == '你是誰':
+#         r = '我是機器人'
+#     elif '訂位' in msg:
+#         r = '你想訂位,是嗎?'
 
 
 # 請 pixabay 幫我們找圖
