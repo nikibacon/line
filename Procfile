6dcd500bcd3c545e045be9f6b1e8a212f0230ac8
NIKI <niki801226@hotmail.com>
2020-08-14 16:26:06 +0800

    rebuild

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web gunicorn app:app
+web: gunicorn app_pixabay:app --preload
 clock: python clock.py
--- app.py
+++ app.py
@@ -52,77 +52,37 @@ def callback():
     return 'OK'
 
 
-# @handler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-#     msg = event.message.text
-#     r = '我看不懂你說什麼'
-
-#     if '貼圖' in msg:
-#         sticker_message = StickerSendMessage(
-#         package_id='1',
-#         sticker_id='1'
-#     )
-#         line_bot_api.reply_message(
-#             event.reply_token,
-#             sticker_message)
-#         return
-
-
-#     if msg in ['hi', 'Hi']:
-#         r = 'hi'
-#     elif msg == '你吃飯了嗎':
-#         r = '還沒'
-#     elif msg == '你是誰':
-#         r = '我是機器人'
-#     elif '訂位' in msg:
-#         r = '你想訂位,是嗎?'
-
- # line_bot_api.reply_message(
- #        event.reply_token,
- #        TextSendMessage(text=r))
-
-
-# 請 pixabay 幫我們找圖
 @handler.add(MessageEvent, message=TextMessage)
-def pixabay_isch(event):
+def handle_message(event):
+    msg = event.message.text
+    r = '我看不懂你說什麼'
 
-        # 先找圖
-        try:
-            url = f"https://pixabay.com/images/search/{urllib.parse.urlencode({'q':event.message.text})[2:]}/"
-            headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
-            
-            req = urllib.request.Request(url, headers = headers)
-            conn = urllib.request.urlopen(req)
-            
-            print('fetch page finish')
-            
-            pattern = 'img srcset="\S*\s\w*,'
-            img_list = []
-            
-            for match in re.finditer(pattern, str(conn.read())):
-                img_list.append(match.group()[12:-3])
-                
-            random_img_url = img_list[random.randint(0, len(img_list)+1)]
-            print('fetch img url finish')
-            print(random_img_url)
-            
-            line_bot_api.reply_message(
-                event.reply_token,
-                ImageSendMessage(
-                    original_content_url=random_img_url,
-                    preview_image_url=random_img_url
-                )
-            )
-        # 找不到圖就學說話
-        except:
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=event.message.text)
-            )
-            pass
+    if '貼圖' in msg:
+        sticker_message = StickerSendMessage(
+        package_id='1',
+        sticker_id='1'
+    )
+        line_bot_api.reply_message(
+            event.reply_token,
+            sticker_message)
+        return
 
 
-   
+    if msg in ['hi', 'Hi']:
+        r = 'hi'
+    elif msg == '你吃飯了嗎':
+        r = '還沒'
+    elif msg == '你是誰':
+        r = '我是機器人'
+    elif '訂位' in msg:
+        r = '你想訂位,是嗎?'
+
+ line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=r))
+
+
+
 
 
 if __name__ == "__main__":
