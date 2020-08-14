e3546dbabc366a6e7c3572a7b7240d576492c49c
NIKI <niki801226@hotmail.com>
2020-08-14 16:08:49 +0800

    clock

--- clock.py
+++ clock.py
@@ -1,9 +1,17 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-
+import datetime
 sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/20')
 def scheduled_job():
+
+    print('========== APScheduler CRON =========')
+    # 馬上讓我們瞧瞧
+    print('This job runs every day */2 min.')
+    # 利用datetime查詢時間
+    print(f'{datetime.datetime.now().ctime()}')
+    print('========== APScheduler CRON =========')
+    
     url = "https://line-94bot.herokuapp.com"
     conn = urllib.request.urlopen(url)
         
