fe7d619c0d6934506988b7d5ccb87f849f8ba983
NIKI <niki801226@hotmail.com>
2020-08-14 17:06:37 +0800

    test

--- app_pixabay.py
+++ app_pixabay.py
@@ -25,7 +25,7 @@ handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
 
 @app.route("/")
 def home():
-    return render_template("home_pixijs.html")
+    return render_template("home.html")
 
 
 # 接收 LINE 的資訊
