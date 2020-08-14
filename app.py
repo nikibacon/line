4e1a55e1a18dcb0b7ded292419ff9ec10b5c302f
NIKI <niki801226@hotmail.com>
2020-08-14 16:46:25 +0800

    test

---
+++ app.py
@@ -0,0 +1,89 @@
+﻿from __future__ import unicode_literals
+import os
+
+from flask import Flask, request, abort
+from flask import render_template
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, StickerSendMessage, FlexSendMessage, ImageSendMessage
+)
+
+import configparser
+
+import urllib
+import re
+import random
+
+
+app = Flask(__name__)
+
+# LINE 聊天機器人的基本資料
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
+handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    msg = event.message.text
+    r = '我看不懂你說什麼'
+
+    if '貼圖' in msg:
+        sticker_message = StickerSendMessage(
+        package_id='1',
+        sticker_id='1'
+    )
+        line_bot_api.reply_message(
+            event.reply_token,
+            sticker_message)
+        return
+
+
+    if msg in ['hi', 'Hi']:
+        r = 'hi'
+    elif msg == '你吃飯了嗎':
+        r = '還沒'
+    elif msg == '你是誰':
+        r = '我是機器人'
+    elif '訂位' in msg:
+        r = '你想訂位,是嗎?'
+
+ line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=r))
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
