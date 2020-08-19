807955b223bdd4e950d8e9a77a4b124ad2d0f455
NIKI <niki801226@hotmail.com>
2020-08-19 15:50:53 +0800

    rebuild

--- models/googleimg.py
+++ models/googleimg.py
@@ -29,42 +29,42 @@ line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
 
 def google_img(event):
 
-    try:
-        try:
-                q_string = {'tbm': 'isch', 'q': event.message.text}
-                url = f"https://www.google.com/search?{urllib.parse.urlencode(q_string)}/"
-                headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'}
-                    
-                req = urllib.request.Request(url, headers = headers)
-                conn = urllib.request.urlopen(req)
-                
-                print('fetch conn finish')
-                    
-                pattern = 'img data-src="\S*"'
-                img_list = []
-                    
-                for match in re.finditer(pattern, str(conn.read())):
-                    img_list.append(match.group()[14:-1])
-                        
-                random_img_url = img_list[random.randint(0, len(img_list)+1)]
-                print('fetch img url finish')
-                print(random_img_url)
-                            
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    ImageSendMessage(
-                        original_content_url=random_img_url,
-                        preview_image_url=random_img_url
-                    )
-                )
-                # 找不到圖就告訴我 user_id
-            except:
-                        
-                r = '我看不懂你在說什麼啦(嘟'
-                line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=r)
-                )
 
-            return True
+    try:
+            q_string = {'tbm': 'isch', 'q': event.message.text}
+            url = f"https://www.google.com/search?{urllib.parse.urlencode(q_string)}/"
+            headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'}
+                
+            req = urllib.request.Request(url, headers = headers)
+            conn = urllib.request.urlopen(req)
+            
+            print('fetch conn finish')
+                
+            pattern = 'img data-src="\S*"'
+            img_list = []
+                
+            for match in re.finditer(pattern, str(conn.read())):
+                img_list.append(match.group()[14:-1])
+                    
+            random_img_url = img_list[random.randint(0, len(img_list)+1)]
+            print('fetch img url finish')
+            print(random_img_url)
+                        
+            line_bot_api.reply_message(
+                event.reply_token,
+                ImageSendMessage(
+                    original_content_url=random_img_url,
+                    preview_image_url=random_img_url
+                )
+            )
+            # 找不到圖就告訴我 user_id
+    except:
+                
+        r = '我看不懂你在說什麼啦(嘟'
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=r)
+        )
+
+    return True
     
