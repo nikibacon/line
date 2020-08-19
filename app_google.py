1add83cea5fe54e8a57a77ccdaab90203a00cff2
NIKI <niki801226@hotmail.com>
2020-08-19 14:53:43 +0800

    add richmenu

--- app_google.py
+++ app_google.py
@@ -49,6 +49,55 @@ def callback():
 
 
 
+headers = {"Authorization":"Bearer b+0MFLF5nMtaQmOU+xRj34thghpJOgcxX5bosFAdRScyP5gHdjzWM3ojFM76bbC7kcvPgU5KXgngWA4DLVVcwxO6R90vmV9+Mt7bN2Y0wF3cwpW14NmVrwySo336EBv3wQ0k1yeMabpFu22akoERrwdB04t89/1O/w1cDnyilFU","Content-Type":"application/json"}
+
+body = {
+    "size": {"width": 2500, "height": 1686},
+    "selected": "true",
+    "name": "Controller",
+    "chatBarText": "Controller",
+    "areas":[
+        {
+          "bounds": {"x": 551, "y": 325, "width": 321, "height": 321},
+          "action": {"type": "message", "text": "up"}
+        },
+        {
+          "bounds": {"x": 876, "y": 651, "width": 321, "height": 321},
+          "action": {"type": "message", "text": "right"}
+        },
+        {
+          "bounds": {"x": 551, "y": 972, "width": 321, "height": 321},
+          "action": {"type": "message", "text": "down"}
+        },
+        {
+          "bounds": {"x": 225, "y": 651, "width": 321, "height": 321},
+          "action": {"type": "message", "text": "left"}
+        },
+        {
+          "bounds": {"x": 1433, "y": 657, "width": 367, "height": 367},
+          "action": {"type": "message", "text": "btn b"}
+        },
+        {
+          "bounds": {"x": 1907, "y": 657, "width": 367, "height": 367},
+          "action": {"type": "message", "text": "btn a"}
+        }
+    ]
+  }
+
+req = requests.request('POST', 'https://api.line.me/v2/bot/richmenu', 
+                       headers=headers,data=json.dumps(body).encode('utf-8'))
+
+print(req.text)
+
+
+with open("control.jpg", 'rb') as f:
+    line_bot_api.set_rich_menu_image("richmenu-c0171d9ce96960d99989ddf29887b7ea", "image/jpeg", f)
+
+req = requests.request('POST', 'https://api.line.me/v2/bot/user/all/richmenu/richmenu-c0171d9ce96960d99989ddf29887b7ea', 
+                       headers=headers)
+
+print(req.text)
+
 # @handler.add(MessageEvent, message=TextMessage)
 # def google_isch(event):
     
